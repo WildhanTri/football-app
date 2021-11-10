de3c2afcdf9156514b9975ad9e5b214cd9c6b29e
Wildhan Triherlian <triherlian@gmail.com>
2021-11-10 18:50:49 +0700

    add search table

--- src/pages/content/home/index.js
+++ src/pages/content/home/index.js
@@ -1,14 +1,9 @@
 
-import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faInfoCircle, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AreaCardItem from "../../../components/AreaCardItem";
-import CompetitionCardItem from "../../../components/CompetitionCardItem";
-import Tables from "../../../components/Tables";
-import TeamCardItem from "../../../components/TeamCardItem";
 import SoccerService from "../../../services/SoccerService";
-import AppBreadcrumb from "../../../shared/AppBreadcrumb";
 import { CHANGE_SELECTED_AREA, SET_COMPETITIONS } from "../../../stores/actions";
 
 const Home = () => {
@@ -16,14 +11,16 @@ const Home = () => {
   var areas = useSelector(state => state.reducer.areas)
   var isLoadingArea = useSelector(state => state.reducer.isLoadingArea)
 
-  useEffect(() => {
-    calculatePages()
-    console.log(pages)
-  }, [areas])
 
   const [page, setPage] = React.useState(1);
   const [offset, setOffset] = React.useState(50);
   const [pages, setPages] = React.useState([]);
+  const [stateSearchInput, setStateSearchInput] = React.useState("");
+
+
+  useEffect(() => {
+    calculatePages(offset)
+  }, [areas, stateSearchInput])
 
   const soccerService = new SoccerService();
 
@@ -55,24 +52,43 @@ const Home = () => {
 
   const onChangePage = (page) => {
     setPage(page)
-    calculatePages()
   }
 
   const calculatePages = (offset) => {
-    const totalPages = Math.ceil(areas.length / offset);
-    const pages = [];
+    const totalPages = Math.ceil(areas.filter(a => stateSearchInput === "" || a.name.toLowerCase().includes(stateSearchInput.toLowerCase())).length / offset);
+    const pagess = [];
     for (let i = 1; i <= totalPages; i++) {
-      pages.push(i);
+      pagess.push(i);
     }
-    setPages(pages)
+
+    setPages(pagess)
   }
 
-
+  const inputOnchangeHandler = (event) => {
+    switch (event.target.id) {
+      case "searchInputContent":
+        setStateSearchInput(event.target.value)
+        break
+      default:
+        break
+    }
+  }
 
   return (
     <div style={styles.container}>
       <div className="container mt-4">
-        <h1 className="mb-4 fw-bold">Select Areas</h1>
+        <div className="d-flex align-items-center mb-2">
+          <div className="flex-grow-1">
+            <h1 className="fw-bold">Select Areas</h1>
+          </div>
+          <div>
+            <div class="input-group">
+              <input type="text" id="searchInputContent" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2" onChange={event => {
+                inputOnchangeHandler(event)
+              }} />
+            </div>
+          </div>
+        </div>
         <div className="table-fix-head scrollbar">
           <table className="table table-condensed table-soccerpedia">
             <thead className="table-head">
@@ -96,7 +112,7 @@ const Home = () => {
                 </tr>
               }
               {
-                !isLoadingArea && areas.slice((page - 1) * offset, page * offset).map((area, index) => {
+                !isLoadingArea && areas.filter(a => a.name.toLocaleLowerCase().includes(stateSearchInput.toLocaleLowerCase())).slice((page - 1) * offset, page * offset).map((area, index) => {
                   return (
                     <tr key={index}>
                       <td>{((page - 1) * offset) + (index + 1)}</td>
@@ -104,7 +120,7 @@ const Home = () => {
                       <td>{area.parentArea}</td>
                       <td>{area.countryCode}</td>
                       <td className="text-end">
-                        <button className="btn btn-primary">
+                        <button className="btn btn-primary" onClick={() => onClickArea(area)}>
                           <FontAwesomeIcon icon={faInfoCircle} />
                         </button>
                       </td>
@@ -112,13 +128,21 @@ const Home = () => {
                   )
                 })
               }
+              {
+                !isLoadingArea && areas.length === 0 &&
+                <tr>
+                  <td className="text-center" colSpan="10">
+                    No Data.
+                  </td>
+                </tr>
+              }
             </tbody>
           </table>
         </div>
 
         <div className="d-flex mt-4">
           <div className="d-flex">
-            <select className="form-select" onChange={(event) => { onChangeOffset(event) }}>
+            <select className="form-select" onChange={(event) => { onChangeOffset(event) }} value={offset}>
               <option value="50">50</option>
               <option value="100">100</option>
               <option value="150">150</option>
@@ -127,20 +151,20 @@ const Home = () => {
           </div>
           <nav className="flex-grow-1">
             <ul className="pagination justify-content-end">
-              <li className="page-item">
-                <a className="page-link" href="#" aria-label="Previous">
+              <li className="page-item" onClick={() => { page > 1 && setPage(page - 1) }}>
+                <a className="page-link" href="javascript:void(0)" aria-label="Previous">
                   <span aria-hidden="true">&laquo;</span>
                 </a>
               </li>
               {
-                pages.map((page, index) => {
+                pages.map((p, index) => {
                   return (
-                    <li className="page-item" onClick={() => { onChangePage(page) }}><a className="page-link" href="#">{page}</a></li>
+                    <li className={['page-item', p === page && 'active'].filter(Boolean).join(' ')} onClick={() => { onChangePage(p) }}><a className="page-link" href="javascript:void(0)">{p}</a></li>
                   )
                 })
               }
-              <li className="page-item">
-                <a className="page-link" href="#" aria-label="Next">
+              <li className="page-item" onClick={() => { page < pages.length && setPage(page + 1) }}>
+                <a className="page-link" href="javascript:void(0)" aria-label="Next">
                   <span aria-hidden="true">&raquo;</span>
                 </a>
               </li>
